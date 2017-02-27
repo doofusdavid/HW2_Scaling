6571b09542c99079ddfdbe0e0f8da5059d114c6b
David Edwards <david.edwards@colostate.edu>
2017-02-27 15:46:45 -0700

    Documentation

--- src/cs455/scaling/util/ServerHashCode.java
+++ src/cs455/scaling/util/ServerHashCode.java
@@ -5,8 +5,17 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Utility Class to process Hash Codes.
+ */
 public class ServerHashCode
 {
+    /**
+     * Calculates SHA1 hash code and returns it.
+     *
+     * @param data Byte array containing a block of data
+     * @return String containing the SHA1 hash
+     */
     public static String SHA1FromBytes(byte[] data)
     {
         MessageDigest digest = null;
