41eecf7c502c2ad50d8b136c14b614c493b671dd
David Edwards <david.edwards@colostate.edu>
2017-02-27 16:12:37 -0700

    changed SentHashList to LinkedList to conform to homework description

--- src/cs455/scaling/client/SentHashList.java
+++ src/cs455/scaling/client/SentHashList.java
@@ -1,20 +1,20 @@
 package cs455.scaling.client;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Maintains the list of Hashes sent to the Server.
  */
 public class SentHashList
 {
-    private final ArrayList<String> sentHashValues = new ArrayList<>();
+    private final LinkedList<String> sentHashValues = new LinkedList<>();
 
     public SentHashList()
     {
     }
 
     /**
-     * Add a Hash to the ArrayList
+     * Add a Hash to the LinkedList
      *
      * @param value The Hash to add
      */
@@ -24,7 +24,7 @@ public class SentHashList
     }
 
     /**
-     * Remove an item from the ArrayList
+     * Remove an item from the LinkedList
      * @param value The Hash to remove
      * @return True if successful, False if no such item was found
      */
